b8b7e26f04bf4088b80aa7e5672d0f631cb0d2d1
shereen-shaaban <sherinshaban67@gmail.com>
2026-05-16 22:59:01 +0300

    feat: refactor registration component to use template reference variables for name and age inputs  only for trying

--- src/component/registretion/registretion.ts
+++ src/component/registretion/registretion.ts
@@ -10,25 +10,38 @@ import {FormsModule} from '@angular/forms'
 })
 export class Registretion {
 
-  stdName:string = "";
-  stdAge:string = "";
-  get AgeNotValid(){
-    return +this.stdAge > 25 || this.stdAge == '0' || +this.stdAge < 0 || Number.isNaN(+this.stdAge)
-  }
-get NameNotValid(){
-  return this.stdName.length <3&& this .stdName!=""
-}
+  // stdName:string = "";
+  // stdAge:string = "";
+//   get AgeNotValid(){
+//     return +this.refAge > 25 || this.stdAge == '0' || +this.stdAge < 0 || Number.isNaN(+this.stdAge)
+//   }
+// get NameNotValid(){
+//   return this.stdName.length <3&& this .stdName!=""
+// }
   @Output() anyevent = new EventEmitter()
 
 
-  addstudents()
-  {
-   if(!this.AgeNotValid && !this.NameNotValid){
-      let stdObj = {name:this.stdName, age:this.stdAge}
+//   addstudents()
+//   {
+//    if(!this.AgeNotValid && !this.NameNotValid){
+//       let stdObj = {name:this.stdName, age:this.stdAge}
+//       this.anyevent.emit(stdObj)
+//       this.stdName = ''
+//       this.stdAge = ''
+//     }
+
+// }
+
+addstudents2(name:string,age:string)
+{
+  //  if(!this.AgeNotValid && !this.NameNotValid){
+      let stdObj = {name:name, age:age}
       this.anyevent.emit(stdObj)
-      this.stdName = ''
-      this.stdAge = ''
-    }
+      // console.log(name,age)
+     name = ''
+     age = ''
+    // }
+
 
 }
 
