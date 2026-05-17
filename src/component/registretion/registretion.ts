aa28d5eb79423b27c5c9448266edb2f1983ae838
shereen-shaaban <sherinshaban67@gmail.com>
2026-05-17 18:14:36 +0300

    feat: enhance registration validation messages for name and age fields

--- src/component/registretion/registretion.ts
+++ src/component/registretion/registretion.ts
@@ -10,41 +10,49 @@ import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} fr
 })
 export class Registretion {
  myform=new FormGroup({
-    name:new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(50)]),
-    age:new FormControl(null,[Validators.min(1),Validators.max(20)])
+    name:new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(50),Validators.pattern('^[A-Za-z].*$')]),
+    age:new FormControl(null,[Validators.min(1),Validators.max(20),Validators.pattern('^[0-9]+$')])
 })
 
 @Output() anyevent = new EventEmitter()
+get namenotexist()
+{
+  return this .myform.controls.name.errors?.['required']&&this.myform.controls.name.touched
+}
+get namepattern()
+{
+  return this.myform.controls.name.errors?.['pattern']?.requiredPattern
+}
+get nameminlength()
+{
+  return this.myform.controls.name.errors?.['minlength']?.requiredLength
+  
+}
+get namemaxlength()
+{
+  return this.myform.controls.name.errors?.['maxlength']?.requiredLength
+  
+}
 get minage()
 {
   return this.myform.controls.age.errors?.['min']
 }
 get maxage()
 {
-  return this.myform.controls.age.errors?.['max']
-}
-get nameexist()
-{
-  return this .myform.controls.name.errors?.['required']&& this.myform.controls.name.value !==null
-}
-get nameminlength()
-{
-  return this.myform.controls.name.errors?.['minlength']?.requiredLength&& this.myform.controls.name.value!==null 
- 
-}
-get namemaxlength()
-{
-  return this.myform.controls.name.errors?.['maxlength'].requiredLength&& this.myform.controls.name.value!==null
- 
+  return this.myform.controls.age.errors?.['max']?.max
 }
 
+get agepattern()
+{
+  return this.myform.controls.age.errors?.['pattern']?.requiredPattern
+}
 addstudents2()
 {
+  console.log(this.myform.controls.name.errors?.['pattern']?.requiredPattern)
   if(this.myform.valid)
 {
 this.anyevent.emit(this.myform.value)
-console.log(this.myform.controls.name.errors?.['required'])
-console.log(this.myform.controls.name.errors?.['minlength'])
+console.log(this.myform.controls.name.value)
 this.myform.reset()
 }
 }
